5637129f40c2000fa2c2070eb1d465a71852095e
Lucas94Sousa <94lucasdss@gmail.com>
2024-02-16 20:02:16 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+    {
+        id:"1",
+        photoCover:"https://t2.tudocdn.net/640125?w=1920",
+        title:"NOVO JOGO DO HOMEM DE FERRO",
+        description:"Homem de Ferro ganhará jogo desenvolvido pela EA e Motive Studio"
+
+    },
+    {
+        id:"2",
+        photoCover:"https://t2.tudocdn.net/640125?w=1920",
+        title:"NOVO JOGO DO HOMEM DE FERRO",
+        description:"Homem de Ferro ganhará jogo desenvolvido pela EA e Motive Studio"
+
+    },
+    {
+        id:"3",
+        photoCover:"https://t2.tudocdn.net/640125?w=1920",
+        title:"NOVO JOGO DO HOMEM DE FERRO",
+        description:"Homem de Ferro ganhará jogo desenvolvido pela EA e Motive Studio"
+
+    },
+    {
+        id:"4",
+        photoCover:"https://t2.tudocdn.net/640125?w=1920",
+        title:"NOVO JOGO DO HOMEM DE FERRO",
+        description:"Homem de Ferro ganhará jogo desenvolvido pela EA e Motive Studio"
+
+    }
+]
